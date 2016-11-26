f2042b686419c4f46a2ece3a5a45232bc35c873a
Maciek Dubinski <maciek@dubinski.net>
2016-11-26 18:09:39 +0100

    Added basic option parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} ARW_FILE [options]", program);
+    println!("arw_info {}\n", VERSION);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
+    println!("{}", input);
 }
