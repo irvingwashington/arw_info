1477cc798b6cd90db8773a69a11ffe753e70495c
Maciek Dubinski <maciek@dubinski.net>
2016-11-30 22:24:42 +0100

    Clean-up namespaces

--- src/arw_file/ifd/ifd_entry.rs
+++ src/arw_file/ifd/ifd_entry.rs
@@ -1,9 +1,9 @@
+use std::mem::transmute;
 use std::fs::File;
 use std::io::Read;
 use std::io::SeekFrom;
 use std::io::Seek;
 use std::fmt;
-use std::mem::transmute;
 
 use arw_file::byte_orders;
 
@@ -23,6 +23,7 @@ pub enum IFDFieldType {
     DOUBLE, // 64b ieee float
     UNKNOWN,
 }
+
 impl IFDFieldType {
     fn from_u16(int_val: u16) -> IFDFieldType {
         if int_val > 0 && int_val < 13 {
@@ -31,13 +32,6 @@ impl IFDFieldType {
             IFDFieldType::UNKNOWN
         }
     }
-}
-
-pub struct IFD {
-    // Image File Directory
-    pub entries_count: u16,
-    pub entries: Vec<IFDEntry>, // 12b x entries_count entries
-    pub next_ifd_offset: u32, // u32 next ifd offset or 0
 }
 
 pub struct IFDEntry {
@@ -79,79 +73,6 @@ impl IFDEntry {
             count: count,
             value_offset: value_offset,
         }
-    }
-}
-
-impl IFD {
-    pub fn new(mut f: &mut File, offset: u32, byte_order: &byte_orders::ByteOrders) -> IFD {
-        let mut buf = vec![0; 4];
-
-        match (*f).seek(SeekFrom::Start(offset as u64)) {
-            Ok(position) => {
-                if position != (offset as u64) {
-                    panic!("Error, can't seek to the offset")
-                }
-            }
-            Err(e) => panic!("Error: {}", e),
-        }
-
-        match (*f).read(&mut buf) {
-            Ok(n) => {
-                if n < 4 {
-                    panic!("IFD incomplete");
-                }
-            }
-            Err(e) => panic!("Error: {}", e),
-        }
-
-        let entries_count = byte_order.parse_u16(&buf[0..2]);
-
-        let mut ifd_entry_offset = offset + 2;
-        let mut entries = vec![];
-
-        for _ in 0..entries_count {
-            entries.push(IFDEntry::new(&mut f, ifd_entry_offset, &byte_order));
-            ifd_entry_offset += 12;
-        }
-
-        match (*f).read(&mut buf) {
-            Ok(n) => {
-                if n < 4 {
-                    panic!("IFD incomplete");
-                }
-            }
-            Err(e) => panic!("Error: {}", e),
-        }
-        let next_ifd_offset = byte_order.parse_u32(&buf[0..4]);
-
-        IFD {
-            entries_count: entries_count,
-            entries: entries,
-            next_ifd_offset: next_ifd_offset,
-        }
-    }
-}
-
-impl fmt::Display for IFD {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "(ArwFile::IFD entries_count: {}, entries num: {}, next_ifd_offset: {}, entries: \
-                {:?})",
-               self.entries_count,
-               self.entries.len(),
-               self.next_ifd_offset,
-               self.entries)
-    }
-}
-
-impl fmt::Display for IFDEntry {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "(ArwFile::IFDEntry tag: {}, field_type: {:?}, count: {}, value_offset: {})",
-               self.tag,
-               self.field_type,
-               self.count,
-               self.value_offset)
     }
 }
 
---
+++ src/arw_file/ifd/mod.rs
@@ -0,0 +1,82 @@
+
+use std::fs::File;
+use std::io::Read;
+use std::io::SeekFrom;
+use std::io::Seek;
+use std::fmt;
+
+use arw_file::byte_orders;
+
+mod ifd_entry;
+mod tag;
+
+pub use self::ifd_entry::IFDEntry;
+
+pub struct IFD {
+    // Image File Directory
+    pub entries_count: u16,
+    pub entries: Vec<IFDEntry>, // 12b x entries_count entries
+    pub next_ifd_offset: u32, // u32 next ifd offset or 0
+}
+
+impl IFD {
+    pub fn new(mut f: &mut File, offset: u32, byte_order: &byte_orders::ByteOrders) -> IFD {
+        let mut buf = vec![0; 4];
+
+        match (*f).seek(SeekFrom::Start(offset as u64)) {
+            Ok(position) => {
+                if position != (offset as u64) {
+                    panic!("Error, can't seek to the offset")
+                }
+            }
+            Err(e) => panic!("Error: {}", e),
+        }
+
+        match (*f).read(&mut buf) {
+            Ok(n) => {
+                if n < 4 {
+                    panic!("IFD incomplete");
+                }
+            }
+            Err(e) => panic!("Error: {}", e),
+        }
+
+        let entries_count = byte_order.parse_u16(&buf[0..2]);
+
+        let mut ifd_entry_offset = offset + 2;
+        let mut entries = vec![];
+
+        for _ in 0..entries_count {
+            entries.push(IFDEntry::new(&mut f, ifd_entry_offset, &byte_order));
+            ifd_entry_offset += 12;
+        }
+
+        match (*f).read(&mut buf) {
+            Ok(n) => {
+                if n < 4 {
+                    panic!("IFD incomplete");
+                }
+            }
+            Err(e) => panic!("Error: {}", e),
+        }
+        let next_ifd_offset = byte_order.parse_u32(&buf[0..4]);
+
+        IFD {
+            entries_count: entries_count,
+            entries: entries,
+            next_ifd_offset: next_ifd_offset,
+        }
+    }
+}
+
+impl fmt::Display for IFD {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "(ArwFile::IFD entries_count: {}, entries num: {}, next_ifd_offset: {}, entries: \
+                {:?})",
+               self.entries_count,
+               self.entries.len(),
+               self.next_ifd_offset,
+               self.entries)
+    }
+}
